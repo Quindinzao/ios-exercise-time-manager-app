c0a2c1a7d1abe75b13e5d5de35d936be313f0c47
Quindinzao <devjaodev@gmail.com>
2025-03-16 01:10:57 -0300

    first commit

--- TimeManager/ViewController.swift
+++ TimeManager/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var outletButton: [UIButton]!
+
+    var timer: [String: Int] = ["walk": 40, "run": 30, "exercise": 60]
+    
     override func viewDidLoad() {
+        for item in outletButton {
+            item.layer.cornerRadius = 28
+        }
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
